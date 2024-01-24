4ae38941e43b52cc1f46567d26bdefe06a7bfd1c
Oga29 <espagambetovoglan@mail.ru>
2024-01-24 23:37:29 +0100

    Added New Assets for Audio Engine

---
+++ RadEngine/Library/Sounds/Wwise_IDs.h
@@ -0,0 +1,63 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID MUSICLOOPPAUSE = 2429473254U;
+        static const AkUniqueID MUSICLOOPPLAY = 2010092982U;
+        static const AkUniqueID MUSICLOOPRESUME = 1319684385U;
+        static const AkUniqueID MUSICLOOPSTOP = 2740630344U;
+        static const AkUniqueID TRAINPAUSE = 3572826029U;
+        static const AkUniqueID TRAINPLAY = 1513415671U;
+        static const AkUniqueID TRAINRESUME = 3760876308U;
+        static const AkUniqueID TRAINSTOP = 201857829U;
+        static const AkUniqueID WINDOWS = 270908044U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID NEW_GAME_PARAMETER = 3671138082U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID EVENT = 3161415855U;
+        static const AkUniqueID MUSIC = 3991942870U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID AUXILIARY_BUS = 1622449088U;
+        static const AkUniqueID NORMAL_BUS = 944811763U;
+    } // namespace AUX_BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
